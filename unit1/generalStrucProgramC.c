c49ade9288a8913743afbd641602935dbd6257cb
elmau21 <elmau0213@gmail.com>
2022-01-28 10:15:23 -0600

    created some variables

---
+++ unit1/generalStrucProgramC.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//* Global Variables*//;
+//Create a variable: dataType nameVariable = anyData;
+//dataType in c/C++/C#/PHP
+
+int unEntero = 3;
+float unDecimal =3.1415;
+char* name = "Mauricio";
+char unCaracter = 'M';
+///////////////////////////////////////
+
+//Ejecuccion del programa///
+
+int main (){
+    printf("Hello Data B %s\n",name);
+    return 0;
+}
