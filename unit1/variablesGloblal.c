a3224f869b8e5d74e95f3f1950a736a358ffdeec
elmau21 <elmau0213@gmail.com>
2022-02-09 10:36:30 -0600

    Added some libraries

---
+++ unit1/variablesGloblal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//* Global Variables*//
+char* date = "02-02-22T1010:25:00z";
+///////////////////////////////////////
+
+//Ejecucion del programa///
+
+int main (){
+    date = "02-03-22T1010:25:00z"
+    int hours = 22;//%Local:main
+    float pi = 3.1415;//%f
+    char character = 'c';//%c
+    char* name = "Mau";//%s
+    int address = &hours; //%p print pointer
+    printf("hours: %d\n", hours, name);
+    return 0;
+}
