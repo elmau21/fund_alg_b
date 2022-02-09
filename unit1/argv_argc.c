48c5dbb858402f0fd761fd068595f49686a50541
elmau21 <elmau0213@gmail.com>
2022-02-09 11:03:56 -0600

    Mofied some archives

---
+++ unit1/argv_argc.c
@@ -0,0 +1,8 @@
+#include <stdio.h>>
+#include <string.h>
+
+int main(int argc, char** argv){
+    printf("Program execute: %s number of inputs elements: %d", argv[0]), argc()
+    printf("Hola %s %s %s %ss, bienvenido!\n", argv[1],argv[2],argv[3]);
+    return 0;
+}
